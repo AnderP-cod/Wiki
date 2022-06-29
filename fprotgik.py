4bcf87740491463cd3396977017d25da451d66a6
AnderP-cod <nemo001leyn@gmail.com>
2022-06-29 14:26:27 +0200

    Update fprotgik.py

--- fprotgik.py
+++ fprotgik.py
@@ -2,7 +2,7 @@ import wikipedia
 import telebot
 import requests
 from bs4 import BeautifulSoup
-
+import webbrowser
 import random
 from telebot import types
 from telegram.ext import Updater, CommandHandler, MessageHandler
@@ -20,12 +20,12 @@ def start_message(wiki):
     item1 = types.KeyboardButton("Зробити пошук")
     item2 = types.KeyboardButton("Рандомна стаття")
     markup.add(item1, item2)
-    bot.send_message(wiki.chat.id, "Доброго дня, що ви хочете зробити", reply_markup=markup)
+    bot.send_message(wiki.chat.id, "Доброго дня! Що ви хочете зробити", reply_markup=markup)
     bot.register_next_step_handler(wiki, start_message_if)
 
 @bot.message_handler(commands=['help'])
 def help_message(help):
-    bot.send_message(help.chat.id, "Телеграм бот вміє робити пошук по\n вікипедії і знаходити рандомну статтю у вікіпедії")
+    bot.send_message(help.chat.id, "Телеграм бот вміє робити пошук по\nвікипедії і знаходити рандомну статтю у вікіпедії")
 
 
 @bot.message_handler(content_types=['text'])
@@ -34,7 +34,7 @@ def start_message_if(wiki):
         bot.send_message(wiki.chat.id, "Напишіть, що ви хочете знайти")
         bot.register_next_step_handler(wiki, wiki_message)
     elif wiki.text == "Рандомна стаття":
-        bot.send_message(wiki.chat.id, "Натисніть 2 рази, щоб спрацювала функція Рандомна стаття з вікіпедії")
+        bot.send_message(wiki.chat.id, "Натисніть 2 рази щоб спрацювала функція Рандомна стаття з вікіпедії")
         bot.register_next_step_handler(wiki, wiki_message_random)
 
 
@@ -54,6 +54,7 @@ def wiki_message_random(wiki):
     title = soup.find(class_="firstHeading").text
     url = "https://en.wikipedia.org/wiki/%s" % title
     bot.send_message(wiki.chat.id, url)
+    webbrowser.open(url)
 
 
 bot.polling(none_stop=True)
